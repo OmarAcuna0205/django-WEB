732913e6d465764c41f3081e3e9f53748fbab6ec
Omar Acuña <13097@ulsachihuahua.edu.mx>
2025-09-22 18:52:20 -0600

    .

--- djanguito/mi_primera_pagina/urls.py
+++ djanguito/mi_primera_pagina/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),              # Hello, World!
-    path('suma/', views.suma, name='suma'),           # Operación matemática
-    path('saludo/', views.saludo_html, name='saludohtml') # Renderiza HTML
-    path('saludo/<str:nombre>/', views.saludo, name='saludo')
+    path('', views.index, name='index'),             
+    path('suma/', views.suma, name='suma'),           
+    path('saludo/', views.saludo_html, name='saludohtml'), 
+    path('saludo/<str:nombre>/', views.saludo, name='Saludo'),
 ]

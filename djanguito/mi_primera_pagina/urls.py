5921161b22d20a56477d64dfa3e80d06a56de01f
Omar Acuña <13097@ulsachihuahua.edu.mx>
2025-09-22 22:43:35 -0600

    modified for a cleaner look

--- djanguito/mi_primera_pagina/urls.py
+++ djanguito/mi_primera_pagina/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index1, name='index1'),             
-    path('suma/<int:a>/<int:b>/', views.suma, name='suma'),
-    path('saludo/', views.saludo_html, name='saludohtml'), 
-    path('<str:nombre>', views.saludo, name='saludo'),
-    path('index/', views.index, name='index'),
+    path('index/', views.index, name='index'),             
     path('index/about/', views.about, name='about'),
-    path('index/suma/', views.suma, name='suma'),
+    path('index/suma/<int:a>/<int:b>/', views.suma, name='suma'),
+    path('<str:nombre>/', views.saludo, name='saludo'),
 ]
--- djanguito/mi_primera_pagina/views.py
+++ djanguito/mi_primera_pagina/views.py
@@ -1,29 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Vista original
-def index1(request):
-    return HttpResponse("Hello, World!")
-
-# Vista con operación matemática (ejemplo: suma)
-def suma(request):
-    a = 5
-    b = 7
-    resultado = a + b
-    return HttpResponse(f"La suma de {a} + {b} es = {resultado}")
-
-# Vista que renderiza un HTML
-def saludo_html(request):
-    contexto = {
-        "nombre": "Omar",
-        "mensaje": "Bienvenido a tu primera página en Django 🚀"
-    }
-    return render(request, "saludo.html", contexto)
-
-def index(request):
+def index (request):
     return render(request, "mi_primera_pagina/index.html")
 
-def about(request):
+def about (request):
     return render(request, "mi_primera_pagina/about.html")
 
 def saludo (request, nombre):

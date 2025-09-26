2a595290281ba4f7aed3ff22e5835002d4b4af86
Omar Acuña <13097@ulsachihuahua.edu.mx>
2025-09-25 19:45:28 -0600

    added class task

--- djanguito/mi_primera_pagina/admin.py
+++ djanguito/mi_primera_pagina/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
 
 # Register your models here.

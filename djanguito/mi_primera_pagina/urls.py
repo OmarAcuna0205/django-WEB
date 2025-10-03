e0f215f5cb5cd87191db977fd348b3d1e226c8b5
Omar Acuña <13097@ulsachihuahua.edu.mx>
2025-10-02 19:09:12 -0600

    Modified urls

--- djanguito/mi_primera_pagina/urls.py
+++ djanguito/mi_primera_pagina/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     
     path('saludo/<str:nombre>/', views.saludo, name='saludo'),
 
-    path('tasks/', views.tasks_index, name='tasks_index'),
+    path('tasks/menu/', views.index2, name='index2'),
+    path('tasks/list/', views.tasks_index, name='tasks_index'),
     path('tasks/add/', views.tasks_add, name='tasks_add'),
     path('tasks/admin/', views.tasks_admin_list, name='tasks_admin_list'),
-    path('tasks/menu/', views.index2, name='index2'),
 ]

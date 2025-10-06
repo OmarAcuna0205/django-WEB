28f8ba047af852ee8c6c4009fd5a60ac77e2296c
Omar Acuña <13097@ulsachihuahua.edu.mx>
2025-10-06 10:23:58 -0600

    new model

--- djanguito/mi_primera_pagina/models.py
+++ djanguito/mi_primera_pagina/models.py
@@ -3,3 +3,11 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+# --- NUEVO MODELO AÑADIDO ---
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
